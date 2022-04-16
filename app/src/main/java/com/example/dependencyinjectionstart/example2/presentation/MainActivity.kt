26c69735bbb9022d8903c6d6e5471fae10c45fc8
nizkiyVorobey <formobileprima@gmail.com>
2022-04-16 13:17:13 +0300

    add example2 module without dagger injection

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -6,14 +6,12 @@ import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ExampleViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val activity = Activity()
-//        activity.keyboard.toString()
-//        activity.mouse.toString()
-//        activity.monitor.toString()
-        activity.computer.toString()
+        viewModel.method()
     }
 }
